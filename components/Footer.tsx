241c8ff64475b6d27f4593d00b849f7b4434559f
langdon <langdon@fedoraproject.org>
2023-03-12 21:11:34 -0400

    converting to typescript

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,10 +1,11 @@
+import { useTheme } from '@mui/material/styles';
 import styles from './Footer.module.css'
 
 export default function Footer() {
   return (
     <>
       <footer className={styles.footer}>
-        <img src="/logo-netlify.svg" alt="Netlify Logo" className={styles.logo} />
+        About Export Sources
       </footer>
     </>
   )
--- components/Header.tsx
+++ components/Header.tsx
@@ -1,3 +1,5 @@
+import { useTheme } from '@mui/material/styles';
+
 export default function Header({ title }) {
   return <h1 className="title">{title}</h1>
 }
