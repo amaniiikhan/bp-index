0ff1be5825b4ec90417e76ac98750565cd629c28
langdon <langdon@fedoraproject.org>
2023-03-13 12:47:35 -0400

    creating placeholders for all detail pages; making links from home page;

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,30 +1,36 @@
 import Head from "next/head";
+import Link from 'next/link'
 import Header from "@components/Header";
 import Footer from "@components/Footer";
 import Grid2 from '@mui/material/Unstable_Grid2';
 import Item from '@mui/material/Unstable_Grid2';
 
-const trends = ["Officer Pay", "Department Salaries", "OT Pay",
-  "Forfeitures per XXX", "Detail Pay", "FIOs per XXX", "Civil Settlements",
-  "Arrest data","Internal Affairs Open Cases"];
+const trends = {
+    "Officer Pay": "police-financial",
+    "Department Salaries": "police-financial",
+    "OT Pay": "police-financial",
+    "Forfeitures per XXX": "forfeiture",
+    "Detail Pay": "police-financial",
+    "FIOs per XXX": "fio",
+    "Civil Settlements": "settlement",
+    "Arrest data": "arrest",
+    "Internal Affairs Open Cases": "internal-affairs"
+  };
 
 export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Next.js Starter!</title>
+        <title>Boston Police Index</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
-        <Header title="Welcome to my app!" />
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
-        </p>
+        <Header title="Welcome to Boston Police Index!" />
         <Grid2 container spacing={{ xs: 2, md: 3 }}>
-          {Array.from(trends).map((subj, index) => (
-            <><Grid2 xs={4} key={index}>
-              <Item>{subj}</Item>
+        {Object.keys(trends).map(key => (
+            <><Grid2 xs={4} key={key}>
+              <Item><Link href={`/details/${encodeURIComponent(trends[key])}`}>{key}</Link></Item>
             </Grid2><Grid2 xs={8}>
                 <Item>trend line</Item>
               </Grid2></>
