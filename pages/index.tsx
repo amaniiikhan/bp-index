b8c25c7733428b9f4e8878f9ae6432b43020f7de
langdon <langdon@fedoraproject.org>
2023-03-12 23:51:01 -0400

    adding basic homepage, placeholdertable, and a first detail page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,14 @@
-import Head from 'next/head'
-import Header from '@components/Header'
-import Footer from '@components/Footer'
+import Head from "next/head";
+import Header from "@components/Header";
+import Footer from "@components/Footer";
+import Grid2 from '@mui/material/Unstable_Grid2';
+import Item from '@mui/material/Unstable_Grid2';
+
+import PlaceholderTable from '@components/PlaceholderTable'
+
+const trends = ["Officer Pay", "Department Salaries", "OT Pay",
+  "Forfeitures per XXX", "Detail Pay", "FIOs per XXX", "Civil Settlements",
+  "Arrest data","Internal Affairs Open Cases"];
 
 export default function Home() {
   return (
@@ -15,9 +23,19 @@ export default function Home() {
         <p className="description">
           Get started by editing <code>pages/index.js</code>
         </p>
+        <PlaceholderTable/>
+        <Grid2 container spacing={{ xs: 2, md: 3 }}>
+          {Array.from(trends).map((subj, index) => (
+            <><Grid2 xs={4} key={index}>
+              <Item>{subj}</Item>
+            </Grid2><Grid2 xs={8}>
+                <Item>trend line</Item>
+              </Grid2></>
+          ))}
+        </Grid2>
       </main>
 
       <Footer />
     </div>
-  )
+  );
 }
