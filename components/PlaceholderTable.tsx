fef854045bbdfbd487f656c2e3b5df971632936e
Simon Kye <kkeysimon@gmail.com>
2023-04-23 20:08:54 -0400

    PlaceholderTable now catches empty json requests

--- components/PlaceholderTable.tsx
+++ components/PlaceholderTable.tsx
@@ -37,69 +37,63 @@ const Placeholder:React.FC<TableData> = ({json}) => {
   let parsedJSON = ""
   try {
     parsedJSON = JSON.parse(JSON.stringify(json))
+    let jsonArr = Object.keys(parsedJSON).map(key => parsedJSON[key])
+    const [data, setData] = React.useState(() => [...jsonArr]);
+    const column = Object.keys(parsedJSON[0]);
+    const ThData = ()=>{
+      return column.map((data)=>{
+        return <TableCell key={data}>{data}</TableCell>
+      })
+    }
+    const downloadCsv = () => {
+      const csvData = json2csv.parse(data); // convert table data to CSV format
+      const blob = new Blob([csvData], { type: 'text/csv;charset=utf-8;' }); // create a Blob object with the CSV data
+      const url = URL.createObjectURL(blob); // create a URL object with the Blob object
+      const link = document.createElement('a'); // create a link element
+      link.setAttribute('href', url); // set the link's href attribute to the URL
+      
+      //We need to change "hello.csv" to something dynamic depending on the current page"
+      link.setAttribute('download', 'hello.csv'); // set the link's download attribute to the filename
+  
+      link.style.display = 'none'; // hide the link
+      document.body.appendChild(link); // add the link to the DOM
+      link.click(); // click the link
+      document.body.removeChild(link); // remove the link from the DOM
+      URL.revokeObjectURL(url); // release the URL object
+    };
+    return (
+      <div>
+      <TableContainer component={Paper}>
+        <Table sx={{ minWidth: 650 }} size="small" aria-label="a dense table">
+          <TableHead>
+            <TableRow key="labels">
+              {ThData()}
+              </TableRow>
+          </TableHead>
+          <TableBody>
+          {data.map((row, index) => (
+            <TableRow key={index}>
+              {column.map((v) => (
+                <TableCell key={`${index}-${v}`}>
+                  {row[v]}
+                </TableCell>
+              ))}
+            </TableRow>
+          ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+      <br/>
+      <Button variant="contained" color="primary" onClick={downloadCsv}>
+        Download CSV
+      </Button>
+      </div>
+    );
   } catch (error) {
       console.error('Invalid JSON:', error.message);
       return <div>Invalid JSON data</div>
   }
-  
-  let jsonArr = Object.keys(parsedJSON).map(key => parsedJSON[key])
-  
-  const [data, setData] = React.useState(() => [...jsonArr]);
-  // const rerender = React.useReducer(() => ({}), {})[1];
 
-  const column = Object.keys(parsedJSON[0]);
-
-  const ThData = ()=>{
-    return column.map((data)=>{
-      return <TableCell key={data}>{data}</TableCell>
-    })
-  }
-
-  const downloadCsv = () => {
-    const csvData = json2csv.parse(data); // convert table data to CSV format
-    const blob = new Blob([csvData], { type: 'text/csv;charset=utf-8;' }); // create a Blob object with the CSV data
-    const url = URL.createObjectURL(blob); // create a URL object with the Blob object
-    const link = document.createElement('a'); // create a link element
-    link.setAttribute('href', url); // set the link's href attribute to the URL
-    
-    //We need to change "hello.csv" to something dynamic depending on the current page"
-    link.setAttribute('download', 'hello.csv'); // set the link's download attribute to the filename
-
-    link.style.display = 'none'; // hide the link
-    document.body.appendChild(link); // add the link to the DOM
-    link.click(); // click the link
-    document.body.removeChild(link); // remove the link from the DOM
-    URL.revokeObjectURL(url); // release the URL object
-  };
-
-  return (
-    <div>
-    <TableContainer component={Paper}>
-      <Table sx={{ minWidth: 650 }} size="small" aria-label="a dense table">
-        <TableHead>
-          <TableRow key="labels">
-            {ThData()}
-            </TableRow>
-        </TableHead>
-        <TableBody>
-        {data.map((row, index) => (
-          <TableRow key={index}>
-            {column.map((v) => (
-              <TableCell key={`${index}-${v}`}>
-                {row[v]}
-              </TableCell>
-            ))}
-          </TableRow>
-        ))}
-        </TableBody>
-      </Table>
-    </TableContainer>
-    <br/>
-    <Button variant="contained" color="primary" onClick={downloadCsv}>
-      Download CSV
-    </Button>
-    </div>
-  );
 }
 
 
