2e22a38f652760219827784921f60d90b22f82fa
Yuyang Wang <uyang68@bu.edu>
2023-04-29 19:36:15 -0400

    real data

--- pages/details/forfeiture.tsx
+++ pages/details/forfeiture.tsx
@@ -15,45 +15,23 @@ Chart.register(
   CategoryScale,
   LinearScale
 );
+function prepareChartData(tableData: any[]) {
+  const assetTypes: { [key: string]: number } = {};
 
-const chartConfig: ChartConfiguration = {
-  type: 'bar',
-  data: {
-    labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-    datasets: [
-      {
-        label: '# of Votes',
-        data: [2, 19, 3, 5, 2, 3],
-        backgroundColor: [
-          'rgba(255, 99, 132, 0.2)',
-          'rgba(54, 162, 235, 0.2)',
-          'rgba(255, 206, 86, 0.2)',
-          'rgba(75, 192, 192, 0.2)',
-          'rgba(153, 102, 255, 0.2)',
-          'rgba(255, 159, 64, 0.2)',
-        ],
-        borderColor: [
-          'rgba(255, 99, 132, 1)',
-          'rgba(54, 162, 235, 1)',
-          'rgba(255, 206, 86, 1)',
-          'rgba(75, 192, 192, 1)',
-          'rgba(153, 102, 255, 1)',
-          'rgba(255, 159, 64, 1)',
-        ],
-        borderWidth: 1,
-      },
-    ],
-  },
-  options: {
-    scales: {
-      y: {
-        beginAtZero: true,
-      },
-      x: {},
-    },
-  },
-  plugins: [],
-};
+  tableData.forEach((item) => {
+    const assetType = item.amount; // Replace 'assetType' with the appropriate key in your data
+    if (assetTypes[assetType]) {
+      assetTypes[assetType] += 1;
+    } else {
+      assetTypes[assetType] = 1;
+    }
+  });
+
+  const labels = Object.keys(assetTypes);
+  const data = labels.map((label) => assetTypes[label]);
+
+  return { labels, data };
+}
 
 
 export const getStaticProps: GetStaticProps = async () => {
@@ -69,6 +47,48 @@ export const getStaticProps: GetStaticProps = async () => {
 
 
 export default function Fio({table}){
+
+  const { labels, data } = prepareChartData(table);
+
+  const chartConfig: ChartConfiguration = {
+    type: 'bar',
+    data: {
+      labels: labels,
+      datasets: [
+        {
+          label: 'Asset Types',
+          data: data,
+          backgroundColor: [
+            'rgba(255, 99, 132, 0.2)',
+            'rgba(54, 162, 235, 0.2)',
+            'rgba(255, 206, 86, 0.2)',
+            'rgba(75, 192, 192, 0.2)',
+            'rgba(153, 102, 255, 0.2)',
+            'rgba(255, 159, 64, 0.2)',
+          ],
+          borderColor: [
+            'rgba(255, 99, 132, 1)',
+            'rgba(54, 162, 235, 1)',
+            'rgba(255, 206, 86, 1)',
+            'rgba(75, 192, 192, 1)',
+            'rgba(153, 102, 255, 1)',
+            'rgba(255, 159, 64, 1)',
+          ],
+          borderWidth: 1,
+        },
+      ],
+    },
+    options: {
+      scales: {
+        y: {
+          beginAtZero: true,
+        },
+        x: {},
+      },
+    },
+    plugins: [],
+  };
+
     return (
         <div>
         <Head>
