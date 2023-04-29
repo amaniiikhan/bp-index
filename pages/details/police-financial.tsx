26d1765c1eb0d2d5a0b911043f8f296f52328fd2
Hou <64312205+SciencePotato@users.noreply.github.com>
2023-04-29 12:11:11 -0400

    Data Visualization "done"

--- pages/details/police-financial.tsx
+++ pages/details/police-financial.tsx
@@ -3,19 +3,165 @@ import Footer from "@components/Footer";
 import { GetStaticProps } from "next";
 import prisma from "lib/prisma";
 import PlaceholderTable from "@components/PlaceholderTable";
+import {data} from '../../utility/2011_2020_bpd_earnings_with_ids.xlsx - 2011_2020_bpd_earnings_with_ids';
+import {roleDataOri} from '../../utility/pd_earning_role'
+import { useEffect, useState } from "react";
+import { Chart as ChartJS, ArcElement, Tooltip, Legend, CategoryScale, LinearScale, BarElement, Title } from 'chart.js';
+import { Pie, Doughnut, Bar } from 'react-chartjs-2';
 
-export const getStaticProps: GetStaticProps = async () => {
-    const feed = await prisma.officer_Pay.findMany();
-    console.log(feed)
-    return {
-      props: {
-        users: JSON.parse(JSON.stringify(feed))
-      }
-    };
-};
 
+// export const getStaticProps: GetStaticProps = async () => {
+//     const feed = await prisma.officer_Pay.findMany();
+//     console.log(feed)
+//     return {
+//       props: {
+//         users: JSON.parse(JSON.stringify(feed))
+//       }
+//     };
+// };
+
+ChartJS.register(ArcElement, Tooltip, Legend);
+
+ChartJS.register(CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend)
 
 export default function Fio({users}){
+  const [combineMap, setCombineMap] = useState<Map<string, number>>(new Map());
+  const [overallData, setOverallData] = useState<object>(null);
+
+  const [roleMap, setRoleMap] = useState<Map<string, number>>(new Map());
+  const [roleData, setRoleData] = useState<object>(null);
+  const [roleOptions, setRoleOptions] = useState<object>(null);
+
+  // Honestly can factorize it into better stuff
+  const handleMapAdd = (tmpMap: Map<string, number>, fieldName: string, obj: object) => {
+      tmpMap.set(fieldName, (tmpMap.get(fieldName) === undefined? 0: tmpMap.get(fieldName)) + parseInt(obj[fieldName] === ""? 0: obj[fieldName]))
+  }
+
+  useEffect(() => {
+    if (combineMap.size === 0) {
+      // Combine Data
+      let tmpMap = combineMap
+      data.map((obj: object) => {
+        handleMapAdd(tmpMap, 'other', obj)
+        handleMapAdd(tmpMap, 'quinn', obj)
+        handleMapAdd(tmpMap, 'injured', obj)
+        handleMapAdd(tmpMap, 'overtime', obj)
+        handleMapAdd(tmpMap, 'regular', obj)
+        handleMapAdd(tmpMap, 'retro', obj)
+        handleMapAdd(tmpMap, 'detail', obj)
+      })
+      setCombineMap(tmpMap)
+
+    } 
+
+    if (roleMap.size === 0) {
+      let tmpMap = roleMap
+      roleDataOri.map((obj: object) => {
+        tmpMap.set(obj['Role'], obj['Yearly Earnings (USD)'])
+      })
+
+      setRoleMap(tmpMap)
+    }
+  
+  }, [])
+
+  useEffect(() => {
+    let tmpData = {
+      'labels': [],
+      'datasets': [
+        {
+          label: 'Combine Dollar vote ',
+          data: [],
+          backgroundColor: [
+            'rgba(255, 99, 132, 0.2)',
+            'rgba(54, 162, 235, 0.2)',
+            'rgba(255, 206, 86, 0.2)',
+            'rgba(75, 192, 192, 0.2)',
+            'rgba(153, 102, 255, 0.2)',
+            'rgba(255, 159, 64, 0.2)',
+            'rgba(255, 255, 255, 0.2)',
+            'rgba(0, 0, 0, 0.2)',
+          ],
+          'borderColor': [
+            'rgba(255, 99, 132, 1)',
+            'rgba(54, 162, 235, 1)',
+            'rgba(255, 206, 86, 1)',
+            'rgba(75, 192, 192, 1)',
+            'rgba(153, 102, 255, 1)',
+            'rgba(255, 159, 64, 1)',
+            'rgba(0, 0, 0, 1)',
+          ],
+          'borderWidth': 1,
+        },
+      ],
+    };
+
+    combineMap.forEach((value, key) => {
+      tmpData['labels'].push(key.charAt(0).toUpperCase() + key.substring(1, key.length))
+      tmpData['datasets'][0]['data'].push(value)
+    })
+
+    setOverallData(tmpData);
+  }, [combineMap])
+
+  useEffect(() => {
+    let options = {
+      responsive: true,
+      plugins: {
+        legend: {
+          position: 'top' as const,
+        },
+        title: {
+          display: true,
+          text: 'Role Average Wage',
+        },
+      },
+    };
+
+    let tmpData = {
+      'labels': [],
+      'datasets': [
+        {
+          label: 'Wage',
+          data: [],
+          backgroundColor: [
+            'rgba(255, 99, 132, 0.2)',
+            'rgba(54, 162, 235, 0.2)',
+            'rgba(255, 206, 86, 0.2)',
+            'rgba(75, 192, 192, 0.2)',
+            'rgba(153, 102, 255, 0.2)',
+            'rgba(255, 159, 64, 0.2)',
+            'rgba(255, 255, 255, 0.2)',
+            'rgba(0, 0, 0, 0.2)',
+          ],
+          'borderColor': [
+            'rgba(255, 99, 132, 1)',
+            'rgba(54, 162, 235, 1)',
+            'rgba(255, 206, 86, 1)',
+            'rgba(75, 192, 192, 1)',
+            'rgba(153, 102, 255, 1)',
+            'rgba(255, 159, 64, 1)',
+            'rgba(0, 0, 0, 1)',
+          ],
+          'borderWidth': 1,
+        },
+      ],
+    };
+
+    roleMap.forEach((value, key) => {
+      tmpData['labels'].push(key.charAt(0).toUpperCase() + key.substring(1, key.length))
+      tmpData['datasets'][0]['data'].push(value)
+    })
+
+    setRoleData(tmpData)
+    setRoleOptions(options)
+  }, [roleMap])
+
     return (
         <div>
         <Head>
@@ -23,9 +169,30 @@ export default function Fio({users}){
             <link rel="icon" href="/favicon.ico" />
         </Head>
 
-        <h1>REPLACE-ME Information</h1>
+        <h1>Police Financial Information</h1>
 
-        <PlaceholderTable json={users}/>
+        {/* <PlaceholderTable json={users}/> */}
+        <div style={{
+          display: 'grid',
+          placeContent: 'center'
+        }}>
+          <div style={{
+            'height': '400px',
+            'width': '800px',
+            'display': 'flex'
+          }}>
+            {overallData && <Pie data={overallData}/>}
+            {overallData && <Doughnut data={overallData}/>}
+          </div>
+
+          <div style={{
+            'height': '400px',
+            'width': '800px'
+          }}>
+              {roleData && <Bar options={roleOptions} data={roleData}/>}
+          </div>
+
+        </div>
 
         <h2>Analysis</h2>
 
