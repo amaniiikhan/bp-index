fce5850c4580164cae8b54a40331a3f453b36c3b
amaniiikhan <amaniak@bu.edu>
2023-05-01 19:46:46 -0400

    taking out placeholder text

--- pages/details/internal-affairs.tsx
+++ pages/details/internal-affairs.tsx
@@ -37,26 +37,7 @@ export default function Fio({users}){
          40.1% not sustained, 31.3% sustained, 15.4% unfounded, 12.8% exonerated, and the 
          remaining 0.4% were withdrawn or filed. 
 
-        ## Explanation
-
-        We don’t serve their kind here!
-        What?
-        Your droids.
-        They’ll have to wait outside.
-        We don’t want them here.
-        Listen, why don’t you wait out by the speeder.
-        We don’t want any trouble.
-        I heartily agree with you sir.
-        Negola dewaghi wooldugger?!?
-        He doesn’t like you.
-        I’m sorry.
-        I don’t like you either You just watch yourself.
-        We’re wanted men.
-        I have the death sentence in twelve systems.
-        I’ll be careful than.
-        You’ll be dead.
-        This little one isn’t worth the effort.
-        Come let me buy you something…
+        
 
 
         <Footer />
